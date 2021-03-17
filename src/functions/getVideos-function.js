a1e1a151d13e32235971fa2b233eae20ee7cf6b3
thicham43 <taroqhicham@gmail.com>
2021-03-17 14:30:45 +0100

    fix: adjust function fetch params

--- src/functions/getVideos-function.js
+++ src/functions/getVideos-function.js
@@ -6,7 +6,7 @@ const CHANNEL_ID = "UCY_5h5zaSwN7Or4kIJDYNXA";
 
 exports.handler = async () => {
   try {
-    const data = await (await fetch(`${this.ROOT_URL}&channelId=${this.CHANNEL_ID}&key=${this.API_KEY}`)).json();
+    const data = await (await fetch(`${ROOT_URL}&channelId=${CHANNEL_ID}&key=${API_KEY}`)).json();
     return {
       statusCode: 200,
       body: JSON.stringify(data),
